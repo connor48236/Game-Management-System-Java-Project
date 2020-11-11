210a238b075f70913620a5afb4c64fbf2f575bd1
Chris Corbett <w0737064@myscc.ca>
2020-11-11 17:12:00 -0500

    LoginExample created

---
+++ src/database/LoginExample.java
@@ -0,0 +1,10 @@
+package database;
+
+public class LoginExample {
+    /*
+        Fill out the information in this file and rename it "Login"
+     */
+    public static final String DB_NAME = "";
+    public static final String DB_USER = "";
+    public static final String DB_PASSWORD = "";
+}
