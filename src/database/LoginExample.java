b9f687e9dd9b730ad4b1dd38ac5f6404153a2674
Chris Corbett <w0737064@myscc.ca>
2020-11-12 10:32:06 -0500

    Added some comments to the LoginExample and Database classes

--- src/database/LoginExample.java
+++ src/database/LoginExample.java
@@ -2,7 +2,8 @@ package database;
 
 public class LoginExample {
     /*
-        Fill out the information in this file and rename it "Login"
+        Fill out the information in this file and rename it "Login" to
+        get the application to work
      */
     public static final String DB_NAME = "";
     public static final String DB_USER = "";
