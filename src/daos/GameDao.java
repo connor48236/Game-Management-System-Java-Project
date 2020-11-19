cc7d3c4fcd237edc24d4f5dab747b7ad20989e8c
Connor Cozad <w0741233@myscc.ca>
2020-11-19 00:41:42 -0500

    Made game Dao for *updateGame *deleteGame *createGame

--- src/daos/GameDao.java
+++ src/daos/GameDao.java
@@ -6,5 +6,8 @@ import java.util.ArrayList;
 
 public interface GameDao {
     ArrayList<Game> getAllGames();
-    Game getGame(int id);
+    public Game getGame(int id);
+    public void updateGame(Game game);
+    public void deleteGame(Game game);
+    public void createGame(Game game);
 }
