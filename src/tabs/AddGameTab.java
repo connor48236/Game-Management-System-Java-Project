92ae9c695789d1b511b3c7c46c3719891fdbd064
Connor Cozad <w0741233@myscc.ca>
2020-11-26 13:44:03 -0500

    *Added text and fields for user to enter in game info

--- src/tabs/AddGameTab.java
+++ src/tabs/AddGameTab.java
@@ -1,10 +1,16 @@
 package tabs;
 
+import javafx.collections.FXCollections;
 import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.Tab;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
+import javafx.scene.text.Text;
 import javafx.stage.FileChooser;
 import main.GameTracker;
+import pojo.Category;
+import pojo.Game;
 import pojo.Platform;
 import tables.CategoryTable;
 import tables.DevInfoTable;
@@ -35,9 +41,36 @@ public class AddGameTab extends Tab {
 
         GridPane root = new GridPane();
 
+        //Text and TextField for adding games
+        Text nameOfGame = new Text("Name Of Game");
+        root.add(nameOfGame, 0, 0);
+        TextField userGameName = new TextField();
+        userGameName.setPromptText("Enter Name of Game here");
+        root.add(userGameName, 1, 0);
+
+        //Text and ComboBox for adding Category
+        Text categoryText = new Text("Category");
+        root.add(categoryText, 2, 0);
+        ComboBox<Category> comboCategory = new ComboBox<>();
+        comboCategory.setItems(FXCollections.observableList(categoryTable.getAllCategories()));
+        root.add(comboCategory, 3, 0);
+
+        //Text and textField for the development company
+        Text developmentCompany = new Text("Enter the Development company");
+        root.add(developmentCompany, 4, 0);
+        TextField userDevCompany = new TextField();
+        userDevCompany.setPromptText("Enter development company here");
+        root.add(userDevCompany, 5, 0);
+
+        //Text and ComboBox for platform
+        Text platform = new Text("Platform");
+        root.add(platform, 6, 0);
+        TextField userPlatform = new TextField();
+        userPlatform.setPromptText("Enter the platforms this game is available on here");
+        root.add(userPlatform, 7, 0);
+
         FileChooser fileChooser = new FileChooser();
         fileChooser.setTitle("Select Image");
-
 
 
         Button imageButton = new Button("Select Image");
@@ -50,7 +83,7 @@ public class AddGameTab extends Tab {
             }
         });
 
-        root.add(imageButton, 0, 0);
+        root.add(imageButton, 8, 0);
 
         this.setContent(root);
     }
