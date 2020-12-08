72cf3551255c2a75cf8cde51af938dd7a19efb51
Connor Cozad <w0741233@myscc.ca>
2020-12-08 11:14:05 -0500

    * Created the table for all the game to be seen in GameLibraryTab *Created The view table to view all the games in a table *added all the columns to the table

--- src/tabs/GameLibraryTab.java
+++ src/tabs/GameLibraryTab.java
@@ -60,31 +60,33 @@ public class GameLibraryTab extends Tab {
         //Adds the column to the table
         gameTableView.getColumns().add(column3);
 
-        //Creates the column of the table for the platform
+        //Creates the column of the table for the Category
         TableColumn<Game, String> column4 = new TableColumn<>("Game Category");
         //Adds the game category to the column
         column4.setCellValueFactory(e -> new SimpleStringProperty(categoryTable.getCategory(e.getValue().getId()).getName()));
         //Adds the column to the table
        gameTableView.getColumns().add(column4);
 
-        //Creates the column of the table for the platform
+        //Creates the column of the table for the release date
         TableColumn<Game, String> column5 = new TableColumn<>("Game Release Date");
         //Adds the game Date to the column
         column5.setCellValueFactory(e -> new SimpleStringProperty(e.getValue().getReleaseDate()));
         //Adds the column to the table
         gameTableView.getColumns().add(column5);
 
-        //Creates the column of the table for the platform
+        //Creates the column of the table for the DevInfo
       TableColumn<Game, String> column6 = new TableColumn<>("Game Developer Info");
-        //Adds the game image to the column
+        //Adds the game dev info to the column
        column6.setCellValueFactory(e -> new SimpleStringProperty("Developer " + devInfoTable.getDevInfo(e.getValue().getId()).getDeveloperName() + " , " +
                "Publisher " + devInfoTable.getDevInfo(e.getValue().getId()).getPublisherName()));
         //Adds the column to the table
        gameTableView.getColumns().add(column6);
 
+       //adds all items to gameTableView
         gameTableView.getItems().addAll(gameTable.getAllGames());
 
 
+        //Added the gameTableView to the Screen
        this.setContent(gameTableView);
     }
 
